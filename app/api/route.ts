f66b9f4ad2df2afa1cd9860a186db3f63f4eb8a7
vlsk3104 <vlsk3104@gmail.com>
2024-01-02 15:16:04 +0900

    初期設定

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,3 +1,8 @@
+/* eslint-disable @typescript-eslint/require-await */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 export const dynamic = 'force-dynamic'
 
 function iteratorToStream(iterator: any) {
